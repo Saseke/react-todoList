7267865dd30049a68ea4679ab253779203c47040
Saseke <717964921@qq.com>
2018-08-16 09:45:02 +0800

    again

--- src/component/Item.js
+++ src/component/Item.js
@@ -15,7 +15,6 @@ class Item extends React.Component {
         onHandleDeleteRecord: PropTypes.func.isRequired
     };
 
-
     componentWillReceiveProps(nextProps) {
         alert("item update");
         if (nextProps.record.text !== this.state.text) {
